165593bbeb4d521b76c0aab6b73444373b4dd6d8
jpvalery <jp.valery@me.com>
2019-07-12 11:20:06 -0400

    📬 Update Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,18 +9,18 @@ const ContactPage = () => (
         <div className={"container"}>
             <div className={"content"}>
                 <div className={"title"}>
-                    <h1>Contact</h1>
+                    <h1>Get in touch</h1>
                 </div>
 
                 <div className={"row"}>
                     <div className={"col-7"}>
-                        <p>We take our commitment to our users seriously. If you need our help with your user account, have questions about how to use the HiStaff, any feedback or are experiencing technical difficulties, please do not hesitate to contact us.</p>
+                        <p>We're friendly and happy to help you figure out our framework.<br />If you need our help, have questions about how to use the UJaaP framework, or would like to hire us to implement it at your company; please do not hesitate to contact us.</p>
                     </div>
 
                     <div className={"col-5"}>
                         <div className={"contact-items"}>
                             <p>Plain email is the best way to contact us.</p>
-                            <h2>contact@histaff.io</h2>
+                            <a href="mailto:contact@ujaap.com?Subject=Contact%20form%20from%20UJaaP.com" rel="noopener noreferrer"><h2>contact@ujaap.com</h2></a>
                         </div>
                     </div>
                 </div>
