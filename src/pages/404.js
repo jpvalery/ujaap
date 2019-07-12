b44c2fa3c39e19f5f3c8f55d14c885b5e13dfa54
jpvalery <jp.valery@me.com>
2019-07-12 11:19:33 -0400

    👻 Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,18 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import thumbnailNotfound from "../../static/images/illustrations/fogg-page-not-found-1.png";
 
 const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found"/>
         <div className={"container"}>
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <h1>Uh... 404</h1>
+            <img alt={"Not Found!"} src={thumbnailNotfound}/>
+            <p>Sorry about that. Looks like this page isn't here anymore (or wasn't even here in the first place).</p>
+            <div className={"button"}>
+                <a href="https://ujaap.com">Return to home</a>
+            </div>
         </div>
     </Layout>
 )
