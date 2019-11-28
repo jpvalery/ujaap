856e5c7fe547a9c57c06e6dbb75be21fbb57acba
jpvalery <jp.valery@me.com>
2019-11-28 11:03:06 -0500

    adding content and folder cleanup

---
+++ content/micro-campaigns.md
@@ -0,0 +1,18 @@
+The development of visual builders and complex campaign management system eventually made obvious issues with such tools. More often than not, the easiness with which you can build a complex map of message makes you forget the end-user experience.
+You end up with a cool looking web of messages and conditions which ends up being a hell to maintain and they don't always make sense for a user.
+**Nobody wins, nobody's happy.**
+
+## Why "micro"?
+Based on the observation above, UJaaP was designed to prohibit such constructs. Following the Core Principles previously edicted, the campaigns can only take a certain shape and size. **A short succession of touchpoints with a clear and focused goal.**
+
+## Do's & Don't's
+
+### It's not a micro campaign if...
+* You have more than 5 touchpoints
+* You're trying to get your users to accomplish multiple goals
+* You're basing your campaign on touchpoints rather than defined goal
+
+### It's a micro campaign if...
+* You identified a goal and benefit from the start
+* Your touchpoints are designed to trigger at the right time and right place
+* The scope is defined in terms of entry, content, and exit
--- src/pages/micro-campaigns.js
+++ src/pages/micro-campaigns.js
@@ -16,7 +16,23 @@ const MicrocampaignsPage = () => (
                   <div className={"title"}>
                       <h1>Micro Campaigns</h1>
                   </div>
-                Lorem ipsum
+                  <p>The development of visual builders and complex campaign management system eventually made obvious issues with such tools. More often than not, the easiness with which you can build a complex map of message makes you forget the end-user experience. You end up with a cool looking web of messages and conditions which ends up being a hell to maintain and they don’t always make sense for a user. <strong>Nobody wins, nobody’s happy.</strong></p>
+                  <h2 id="why-micro">Why “micro”?</h2>
+                  <p>Based on the observation above, UJaaP was designed to prohibit such constructs. Following the Core Principles previously edicted, the campaigns can only take a certain shape and size. <strong>A short succession of touchpoints with a clear and focused goal.</strong></p>
+                  <h2 id="dos-donts">Do’s &amp; Don’t’s</h2>
+                  <h3 id="its-not-a-micro-campaign-if">It’s not a micro campaign if…</h3>
+                  <ul>
+                  <li>You have more than 5 touchpoints</li>
+                  <li>You’re trying to get your users to accomplish multiple goals</li>
+                  <li>You’re basing your campaign on touchpoints rather than defined goal</li>
+                  </ul>
+                  <h3 id="its-a-micro-campaign-if">It’s a micro campaign if…</h3>
+                  <ul>
+                  <li>You identified a goal and benefit from the start</li>
+                  <li>Your touchpoints are designed to trigger at the right time and right place</li>
+                  <li>The scope is defined in terms of entry, content, and exit</li>
+                  </ul>
+
 
                 <div className={"post-nav"}>
 
